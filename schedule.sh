28f64e46f6d74a2f6271ccd8e7fdb61ffd0bfe8d
Michaelliem99 <43264172+Michaelliem99@users.noreply.github.com>
2023-03-20 21:49:45 +0700

    Update virtualenv python path in schedule.sh

--- schedule.sh
+++ schedule.sh
@@ -2,10 +2,10 @@
 now=$(date)
 echo "Start Time: $now" &&
 echo "Start IDX Stocks" &&
-/root/anaconda3/envs/web_scraping/bin/python "01 - IDX Stocks.py" &&
+/home/michaelliem99/anaconda3/envs/web_scraping/bin/python "01 - IDX Stocks.py" &&
 echo "Start IDX Stock Sectoral and Index" &&
-/root/anaconda3/envs/web_scraping/bin/python "02 - IDX Stock Sectoral and Index.py" &&
+/home/michaelliem99/anaconda3/envs/web_scraping/bin/python "02 - IDX Stock Sectoral and Index.py" &&
 echo "Start IDX KSEI Bonds" &&
-/root/anaconda3/envs/web_scraping/bin/python "03 - KSEI Bonds.py" &&
+/home/michaelliem99/anaconda3/envs/web_scraping/bin/python "03 - KSEI Bonds.py" &&
 now=$(date)
 echo "End Time: $now"
