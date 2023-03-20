f1f94fd1cac61961acd6fd702675f2fdff496eac
Michaelliem99 <43264172+Michaelliem99@users.noreply.github.com>
2023-03-20 19:09:23 +0700

    Update schedule.sh script to add some console output

--- schedule.sh
+++ schedule.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 now=$(date)
-echo "Start Time: $now"
-/root/anaconda3/envs/web_scraping/bin/python "01 - IDX Stocks.py"
-/root/anaconda3/envs/web_scraping/bin/python "02 - IDX Stock Sectoral and Index.py"
-/root/anaconda3/envs/web_scraping/bin/python "03 - KSEI Bonds.py"
+echo "Start Time: $now" &&
+echo "Start IDX Stocks" &&
+/root/anaconda3/envs/web_scraping/bin/python "01 - IDX Stocks.py" &&
+echo "Start IDX Stock Sectoral and Index" &&
+/root/anaconda3/envs/web_scraping/bin/python "02 - IDX Stock Sectoral and Index.py" &&
+echo "Start IDX KSEI Bonds" &&
+/root/anaconda3/envs/web_scraping/bin/python "03 - KSEI Bonds.py" &&
 now=$(date)
 echo "End Time: $now"
